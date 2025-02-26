d3dfa731a09e367ba2bffe1253b18bd99d119c98
Ramy Ouda <ramyouda@gmail.com>
2025-02-26 15:47:50 -0500

    add refetch button and improve stylesheet functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import {
   Component,
   inject,
   OnInit,
+  signal,
 } from '@angular/core';
+import { map, takeWhile, timer } from 'rxjs';
 import { AppStore } from './store/app.store';
 
 @Component({
@@ -20,7 +22,23 @@ export class AppComponent implements OnInit {
   readonly advice = this.#store.advice;
   readonly loading = this.#store.loading;
 
+  readonly timer = signal<number>(0);
+
   ngOnInit(): void {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice(): void {
     this.#store.fetchAdvice();
+    this.startAdviceTimer();
+  }
+
+  private startAdviceTimer(): void {
+    timer(0, 1000)
+      .pipe(
+        takeWhile((tick) => tick <= 3),
+        map((tick) => 3 - tick)
+      )
+      .subscribe((tick) => this.timer.set(tick));
   }
 }
--- src/app/store/app.service.ts
+++ src/app/store/app.service.ts
@@ -2,15 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
+interface AdviceResponse {
+  slip: AdviceData;
+}
+
+interface AdviceData {
+  id: number;
+  advice: string;
+}
+
 @Injectable({ providedIn: 'root' })
 export class AppService {
   readonly #http = inject(HttpClient);
 
-  fetchAdvice(): Observable<{ id: number; advice: string }> {
+  fetchAdvice(): Observable<AdviceData> {
     return this.#http
-      .get<{ slip: { id: number; advice: string } }>(
-        'https://api.adviceslip.com/advice'
-      )
+      .get<AdviceResponse>('https://api.adviceslip.com/advice')
       .pipe(map(({ slip }) => slip));
   }
 }
